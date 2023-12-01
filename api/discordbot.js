53e5f52f4d8d8a7e6ca4a782ffdc0e026ede878c
Ying-Kai Liao <yklworkingaddress@gmail.com>
2023-12-01 15:30:28 +0800

    form try

--- api/discordbot.js
+++ api/discordbot.js
@@ -1,28 +1,49 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const channelID = process.env.CHANNEL_ID
-const token = process.env.TOKEN
+const { Client, GatewayIntentBits, AttachmentBuilder } = require("discord.js");
+const formidable = require("formidable");
+const channelID = process.env.CHANNEL_ID;
+const token = process.env.TOKEN;
 
 module.exports = async (req, res) => {
-  if (req.method === 'POST') { // Change to 'GET' if you prefer
+  if (req.method === "POST") {
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        console.error("Error parsing the form:", err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      // Process the file here
+      // For simplicity, assuming there's one file and it's available as 'files.fileToUpload'
+      const file = files.fileToUpload;
+
+      // Initialize Discord client and send the file
       const client = new Client({
-          intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
+        intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
       });
 
-      client.once('ready', async () => {
-          try {
-              // Example: Send a message to a specific channel
-              const channel = await client.channels.fetch(channelID);
-              await channel.send('Hello from the bot!');
-              client.destroy();
-              res.status(200).send('Message sent successfully');
-          } catch (error) {
-              client.destroy();
-              res.status(500).send('An error occurred');
-          }
+      client.once("ready", async () => {
+        try {
+          const channel = await client.channels.fetch(channelID);
+          const message = await channel.send({
+            content: "Here is the uploaded file:",
+            files: [new AttachmentBuilder(file.filepath)],
+          });
+
+          client.destroy();
+          res.status(200).json({ messageInfo: message });
+        } catch (error) {
+          console.error("Bot error:", error);
+          client.destroy();
+          res.status(500).send("An error occurred with the bot");
+        }
       });
 
       client.login(token);
+    });
+
   } else {
-      res.status(405).send('Method Not Allowed');
+    res.status(405).send("Method Not Allowed");
   }
 };
