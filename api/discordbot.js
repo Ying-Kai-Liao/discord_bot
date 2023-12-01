30dddcec9b55245e7b5b306bf73f254093c93e90
Ying-Kai Liao <yklworkingaddress@gmail.com>
2023-12-01 16:11:34 +0800

    serialization of message

--- api/discordbot.js
+++ api/discordbot.js
@@ -3,6 +3,43 @@ const formidable = require("formidable");
 const channelID = process.env.CHANNEL_ID;
 const token = process.env.TOKEN;
 
+function serializeMessage(message) {
+  return {
+      channelId: message.channelId,
+      guildId: message.guildId,
+      id: message.id,
+      createdTimestamp: message.createdTimestamp,
+      type: message.type,
+      system: message.system,
+      content: message.content,
+      author: {
+          id: message.author.id,
+          bot: message.author.bot,
+          system: message.author.system,
+          flags: message.author.flags.bitfield,
+          username: message.author.username,
+          discriminator: message.author.discriminator,
+          avatar: message.author.avatar,
+          banner: message.author.banner,
+          accentColor: message.author.accentColor
+      },
+      pinned: message.pinned,
+      tts: message.tts,
+      nonce: message.nonce,
+      embeds: message.embeds.map(embed => embed.toJSON()),
+      components: message.components.map(component => component.toJSON()),
+      attachments: Array.from(message.attachments.values()).map(attachment => ({
+          id: attachment.id,
+          url: attachment.url,
+          name: attachment.name,
+          size: attachment.size,
+          height: attachment.height,
+          width: attachment.width,
+          contentType: attachment.contentType
+      }))
+  };
+}
+
 module.exports = async (req, res) => {
   if (req.method === "POST") {
     const form = new formidable.IncomingForm();
@@ -35,8 +72,10 @@ module.exports = async (req, res) => {
 
           const fetchedMessage = await channel.messages.fetch(sentMessage.id);
 
+          const serializedMessage = serializeMessage(fetchedMessage);
+
           client.destroy();
-          res.status(200).json({ messageInfo: fetchedMessage });
+          res.status(200).json({ messageInfo: serializedMessage });
         } catch (error) {
           console.error("Bot error:", error);
           client.destroy();
