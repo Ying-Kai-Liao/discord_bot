c1b328a54efffa0d3d27bf2a1f5701050b76c3bc
Ying-Kai Liao <yklworkingaddress@gmail.com>
2023-12-14 19:15:57 +0800

    CORS update

--- api/discordbot.js
+++ api/discordbot.js
@@ -4,6 +4,21 @@ const channelID = process.env.CHANNEL_ID;
 const token = process.env.TOKEN;
 
 module.exports = async (req, res) => {
+  // Set CORS headers for localhost:3000
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000", "https://chiangs-site.vercel.app/"); // Only allow requests from localhost:3000
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,POST");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Handle OPTIONS request (CORS preflight)
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method === "POST") {
     const form = new formidable.IncomingForm();
 
@@ -31,7 +46,7 @@ module.exports = async (req, res) => {
       client.once("ready", async () => {
         try {
           const channel = await client.channels.fetch(channelID);
-          const content = `Email: ${email}\nFilename: ${fileData.originalFilename}`
+          const content = `Email: ${email}\nFilename: ${fileData.originalFilename}`;
           const sentMessage = await channel.send({
             content,
             files: [attachment],
@@ -47,7 +62,6 @@ module.exports = async (req, res) => {
           } else {
             res.status(500).send("Failed to send the file");
           }
-
         } catch (error) {
           client.destroy();
           res.status(500).send("An error occurred with the bot");
